a29ba9bcdaec609b8dace0ef3286169d5f002987
Aaron Jones Rails <jones@rmrdevelopment.com>
2016-04-12 20:21:29 +0000

    Add show and gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,12 @@ class UsersController < ApplicationController
         
     end
     
+    def show
+        
+        @user = User.find(params[:id])
+        
+    end
+    
     
     private
     def user_params
